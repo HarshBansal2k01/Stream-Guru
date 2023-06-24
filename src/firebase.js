3d1f7e628aee2b970a60348e550380ab39e117a3
HarshBansal2k01 <ihbbro14@gmail.com>
2023-06-24 22:42:16 +0530

    done with fetching and hosting

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAM3dr38W882G-i9NuQrLMSEvIiEVo9fAE",
+    authDomain: "stream-guru.firebaseapp.com",
+    projectId: "stream-guru",
+    storageBucket: "stream-guru.appspot.com",
+    messagingSenderId: "817124369279",
+    appId: "1:817124369279:web:95d5210ae5096cce31b04a"
+  };
